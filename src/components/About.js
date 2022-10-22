2a724a69cc277a5717fd8b2660dcab8c0d7cee38
Domaine Scully <dalexscully@gmail.com>
2022-10-22 16:37:35 -0400

    Lab05 completed

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ import vueIcon from "@iconify/icons-logos/vue";
 class About extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
-      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+      var businesspic = "images/" + this.props.sharedBasicInfo.image;
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
@@ -27,7 +27,7 @@ class About extends Component {
                 <span style={{ cursor: "auto" }}>
                   <img
                     height="250px"
-                    src={profilepic}
+                    src={businesspic}
                     alt="Avatar placeholder"
                   />
                   <Icon
